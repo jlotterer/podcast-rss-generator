8a4c0a01258c579fcb9b9a9f1e581d845995c7ef
jlotterer <74730619+jlotterer@users.noreply.github.com>
2025-09-19 14:20:35 -0400

    mod next.config.js to allow images from github

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        // This allows images from your GitHub repository to be displayed.
+        // If your image is hosted on a different domain, update the hostname.
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
