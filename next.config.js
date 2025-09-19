9906e57aa53c19405f5ab39a08f55482479d3fc3
jlotterer <74730619+jlotterer@users.noreply.github.com>
2025-09-19 13:03:22 -0400

    added headers section to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        // This ensures your cover art is publicly accessible
+        source: '/podcast-cover.png',
+        headers: [
+          {
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
