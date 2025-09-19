7ad214121bbdfcc399e55027c5ce60f8b7d06308
jlotterer <74730619+jlotterer@users.noreply.github.com>
2025-09-19 14:28:36 -0400

    updated var

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        // This allows images from your GitHub repository to be displayed.
-        // If your image is hosted on a different domain, update the hostname.
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
